dbd20b47a89eb29f156829c5a7b66f468383704d
Sam <splavigne@gmail.com>
2017-11-06 13:19:28 -0500

    search for binary

--- vidpy/__init__.py
+++ vidpy/__init__.py
@@ -1,4 +1,18 @@
-MELT_BINARY = '/Applications/Shotcut.app/Contents/MacOS/qmelt'
+import os
+
+MELT_BINARY = ''
+
+MAC = '/Applications/Shotcut.app/Contents/MacOS/qmelt'
+WIN = 'C:\Program Files\Shotcut'
+
+if os.path.exists(MAC):
+    MELT_BINARY = MAC
+elif os.path.exists(WIN):
+    MELT_BINARY = WIN
+else:
+    MELT_BINARY = 'melt'
+
+
 
 from .clip import Clip
 from .composition import Composition
