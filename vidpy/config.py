7fe4d9e50071c67dcff92961460efcd946398ef4
Sam <splavigne@gmail.com>
2017-11-06 21:46:54 -0500

    Fix vidpy windows support

--- vidpy/config.py
+++ vidpy/config.py
@@ -3,7 +3,7 @@ import os
 MELT_BINARY = ''
 
 MAC = '/Applications/Shotcut.app/Contents/MacOS/qmelt'
-WIN = 'C:\Program Files\Shotcut'
+WIN = 'C:\Program Files\Shotcut\qmelt.exe'
 
 if os.path.exists(MAC):
     MELT_BINARY = MAC
