b5b10c1dadf440fd51e30d6a1518f277d21cdf66
Sam <splavigne@gmail.com>
2017-11-07 18:17:04 -0500

    add todos

--- tests/test_composition.py
+++ tests/test_composition.py
@@ -2,6 +2,16 @@ import unittest
 from vidpy import Composition, config, Clip
 
 class TestComposition(unittest.TestCase):
+    '''
+    TODO:
+        preview()
+        xml()
+        save()
+        save_xml()
+        duration tests
+        width/height/fps tests
+    '''
+
     def test_empty_composition(self):
         config.MELT_BINARY = 'melt'
         comp = Composition([])
@@ -29,6 +39,8 @@ class TestComposition(unittest.TestCase):
         clip2 = Clip('video2.mp4')
         comp = Composition([clip, clip2])
         self.assertEqual(str(comp), 'melt -track color:#000000 out=0 -track video.mp4 in=":0.000000" -track video2.mp4 in=":0.000000" -transition composite distort=1 a_track=0 b_track=1 -transition mix a_track=0 b_track=1 -transition composite distort=1 a_track=0 b_track=2 -transition mix a_track=0 b_track=2')
+
+
 
 
 if __name__ == '__main__':
