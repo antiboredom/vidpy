af8066505e9b1699109673028c0fac84f67d71c6
Sam <splavigne@gmail.com>
2017-11-14 12:06:11 -0500

    update mask example

--- demos/mask.py
+++ demos/mask.py
@@ -4,10 +4,10 @@ background_clip = Clip('videos/hand1.mp4')
 clip = Clip('videos/hand2.mp4')
 
 # create a clip to use a as a mask
-# you could use use a video for this or a text clip
+# you can use use a video,text clip or an image
 mask = Clip('videos/mask.png')
 
-# you can add effects to the mask
+# you can also add effects to the mask
 mask.glow('0=0;200=.9')
 
 # make sure that the duration of the mask is the same as the clip
