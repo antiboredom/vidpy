19e1dbafc8d70718085b39534131adea407e2d3a
Sam Lavigne <splavigne@gmail.com>
2023-08-16 15:21:27 -0400

    fix overlay demo

--- demos/overlay.py
+++ demos/overlay.py
@@ -1,7 +1,7 @@
 import glob
 from vidpy import Composition, Clip
 
-clips = [Clip(v) for v in glob.glob('/Users/sam/Downloads/test*.mov')]
+clips = [Clip(v) for v in glob.glob('./videos/*.mp4')]
 clips = [c.opacity(.5) for c in clips]
 
 Composition(clips).preview()
